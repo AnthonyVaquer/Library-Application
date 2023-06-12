f0b184e786fd45f75bd83e06e80bc4106a424782
Anthony Vaquer <anthonymark126@gmail.com>
2023-06-11 22:34:10 -0700

    apollo

--- Develop/client/src/index.js
+++ Develop/client/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import { ApolloClient, ApolloProvider } from "@apollo/client";
+
+var client = new ApolloClient({
+  uri: "http://localhost:3010",
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client = { client }>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
